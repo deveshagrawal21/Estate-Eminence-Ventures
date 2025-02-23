7ad0d6707c01fdabda7b50a011755a7994555f1c
deveshagrawal21 <deveshsatishagrawal7@gmail.com>
2025-02-23 17:02:36 +0000

    update

--- estate-eminence-ventures/src/reportWebVitals.ts
+++ estate-eminence-ventures/src/reportWebVitals.ts
@@ -3,10 +3,19 @@ import { ReportHandler } from 'web-vitals';
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Measure Cumulative Layout Shift (CLS)
       getCLS(onPerfEntry);
+
+      // Measure First Input Delay (FID)
       getFID(onPerfEntry);
+
+      // Measure First Contentful Paint (FCP)
       getFCP(onPerfEntry);
+
+      // Measure Largest Contentful Paint (LCP)
       getLCP(onPerfEntry);
+
+      // Measure Time to First Byte (TTFB)
       getTTFB(onPerfEntry);
     });
   }
